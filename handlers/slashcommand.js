570a6131aa0dba0e523208daec03995d5804a4e0
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-10 23:14:15 +0700

    Update slashcommand.js

--- handlers/slashcommand.js
+++ handlers/slashcommand.js
@@ -2,7 +2,6 @@ const { readdirSync } = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 require('dotenv').config();
-const { Client } = require('discord.js');
 
 /**
    *
@@ -10,36 +9,38 @@ const { Client } = require('discord.js');
    */
 
 module.exports = (client) => {
-    try {
-        //import env files
-        const clientId = process.env.DISCORD_CLIENTID
-        const token = process.env.DISCORD_TOKEN
-        //make a variable
-        const slashCommands = [];
-        //read file
-        readdirSync("./slashcommands").forEach(dir => {
-            let slscommands = readdirSync(`./slashcommands/${dir}/`).filter((file) => file.endsWith(".js"));
-            for (let file of slscommands) {
-                let command = require(`../slashcommands/${dir}/${file}`);
-                slashCommands.push(command.data.toJSON());
-                if (command.data.name) {
-                    client.slashCommands.set(command.data.name, command);
-                    console.log(file, '- Success')
-                } else {
-                    console.log(file, `- Error`);
-                }
-            }
-        });
+	try {
+		// import env files
+		const clientId = process.env.DISCORD_CLIENTID;
+		const token = process.env.DISCORD_TOKEN;
+		// make a variable
+		const slashCommands = [];
+		// read file
+		readdirSync('./slashcommands').forEach(dir => {
+			const slscommands = readdirSync(`./slashcommands/${dir}/`).filter((file) => file.endsWith('.js'));
+			for (const file of slscommands) {
+				const command = require(`../slashcommands/${dir}/${file}`);
+				slashCommands.push(command.data.toJSON());
+				if (command.data.name) {
+					client.slashCommands.set(command.data.name, command);
+					console.log(file, '- Success');
+				}
+				else {
+					console.log(file, '- Error');
+				}
+			}
+		});
 
-        const rest = new REST({ version: "9" }).setToken(token);
-        rest.put(
-            Routes.applicationCommands(clientId),
-            {
-                body: slashCommands,
-            },
-        );
-        console.log('SlashCommand is ready');
-    } catch (e) {
-        console.log(e);
-    }
-}
+		const rest = new REST({ version: '9' }).setToken(token);
+		rest.put(
+			Routes.applicationCommands(clientId),
+			{
+				body: slashCommands,
+			},
+		);
+		console.log('SlashCommand is ready');
+	}
+	catch (e) {
+		console.log(e);
+	}
+};
