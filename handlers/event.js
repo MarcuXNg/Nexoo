a707c7cb004672b90461a6e98711bf4d6a25e2e7
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-10 23:13:53 +0700

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { Client } = require('discord.js');
 
 /**
    *
@@ -7,26 +6,29 @@ const { Client } = require('discord.js');
    */
 
 module.exports = (client) => {
-    try {
-        fs.readdirSync("./events/").forEach((dir) => {
-            const events = fs.readdirSync(`./events/${dir}/`).filter(file => file.endsWith('.js'));
+	try {
+		fs.readdirSync('./events/').forEach((dir) => {
+			const events = fs.readdirSync(`./events/${dir}/`).filter(file => file.endsWith('.js'));
 
-            for (const file of events) {
-                const pull = require(`../events/${dir}/${file}`);
-                if (pull.name) {
-                    client.events.set(pull.name, pull)
-                } else {
-                    continue;
-                }
-                if (pull.once) {
-                    client.once(pull.name, (...args) => pull.run(...args));
-                } else {
-                    client.on(pull.name, (...args) => pull.run(...args));
-                }
-            }
-        });
-        console.log(`Events Handler is ready`)   
-    } catch (e) {
-        console.log(e);
-    }
-}
+			for (const file of events) {
+				const pull = require(`../events/${dir}/${file}`);
+				if (pull.name) {
+					client.events.set(pull.name, pull);
+				}
+				else {
+					continue;
+				}
+				if (pull.once) {
+					client.once(pull.name, (...args) => pull.run(...args));
+				}
+				else {
+					client.on(pull.name, (...args) => pull.run(...args));
+				}
+			}
+		});
+		console.log('Events Handler is ready');
+	}
+	catch (e) {
+		console.log(e);
+	}
+};
