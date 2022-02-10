d261f3417752ca77356ea1aeb3beeecef1d81934
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-10 23:10:43 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,25 +1,30 @@
-const config = require('./config.json');
 const Discord = require('discord.js');
 const checkSameRoom = (message) => {
-        //send error if member is Deafed
-        if (message.member.voice.selfDeaf) return message.channel.send({
-            embeds: [new Discord.MessageEmbed().setColor("RANDOM").setDescription(`❌| **You cannot run this command while deafened**`)]
-        });
-        //check if the voicechannel is full or not
-        const voiceChannel = message.member.voice.channel;
-        if (voiceChannel.full) return message.channel.send({
-            embeds: [
-                new Discord.MessageEmbed().setColor("RANDOM").setDescription(`❌ | the channel is full!`)]
-        });
-        if (!message.member.voice.channel) return message.channel.send({
-            embeds: [new Discord.MessageEmbed().setColor("RANDOM").setDescription(`You need to be in a voice channel to use the bot!`)]
-        });
-        if (!message.guild.me.voice.channel || message.guild.me.voice.channel.id == message.member.voice.channel.id) return;
-        return message.channel.send({
-            embeds: [new Discord.MessageEmbed().setColor("RANDOM").setTitle(`🤷‍♂️I'm connected to somewhere else`).setDescription(`You need to be in the same voice channel with the me to use the command!`)]
-        });
-}
+	// send error if member is Deafed
+	if (message.member.voice.selfDeaf) {
+		return message.channel.send({
+			embeds: [new Discord.MessageEmbed().setColor('RANDOM').setDescription('❌| **You cannot run this command while deafened**')],
+		});
+	}
+	// check if the voicechannel is full or not
+	const voiceChannel = message.member.voice.channel;
+	if (voiceChannel.full) {
+		return message.channel.send({
+			embeds: [
+				new Discord.MessageEmbed().setColor('RANDOM').setDescription('❌ | the channel is full!')],
+		});
+	}
+	if (!message.member.voice.channel) {
+		return message.channel.send({
+			embeds: [new Discord.MessageEmbed().setColor('RANDOM').setDescription('You need to be in a voice channel to use the bot!')],
+		});
+	}
+	if (!message.guild.me.voice.channel || message.guild.me.voice.channel.id == message.member.voice.channel.id) return;
+	return message.channel.send({
+		embeds: [new Discord.MessageEmbed().setColor('RANDOM').setTitle('🤷‍♂️I\'m connected to somewhere else').setDescription('You need to be in the same voice channel with the me to use the command!')],
+	});
+};
 
 module.exports = {
-    checkSameRoom,
-}
+	checkSameRoom,
+};
