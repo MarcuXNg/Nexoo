203aef1290383f0245a31d0d4f962bffb51c05e7
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-12 13:19:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.distube = new DisTube(client, {
 	savePreviousSongs: true,
 	leaveOnEmpty: true,
 	emptyCooldown: 30,
-	nsfw: true,
+	nsfw: false,
 	leaveOnFinish: true,
 	leaveOnStop: true,
 	youtubeDL: false,
