de0310ba0ce462b10b734f05b6d981b68031013a
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-12 14:04:28 +0700

    Update distubeevent.js

--- events/distube/distubeevent.js
+++ events/distube/distubeevent.js
@@ -13,7 +13,7 @@ client.distube
 				.setColor('RANDOM')
 				.setThumbnail(`${song.thumbnail}`)
 				.setAuthor('Now playing', config.iconURL)
-				.setDescription(`**[${song.name}](${song.url})** \n ${status(queue)} \n ╰🔉VoiceChannel: <#${client.VoiceChannel.id}>`)
+				.setDescription(`**[${song.name}](${song.url})** \n ${status(queue)} \n ╰🔉VoiceChannel: <#${voiceChannel.id}>`)
 				.setFields(
 					{
 						name: '╰🙆‍♂️ Requested By',
