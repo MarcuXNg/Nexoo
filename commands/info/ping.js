04c0008fc886d732462415b3577e69aa39acd4cb
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-12 14:13:12 +0700

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,23 +1,24 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
 module.exports = {
-    name: "ping",
-    aliases: ['ping'],
-    category: 'info',
-    description: 'show ping in ms',
-    usage: `${config.prefix}ping`,
-    run: (client, message, args) => {
-        try {
-            const embed = new Discord.MessageEmbed();
+	name: 'ping',
+	aliases: ['ping'],
+	category: 'info',
+	description: 'show ping in ms',
+	usage: `${config.prefix}ping`,
+	run: (client, message, args) => {
+		try {
+			const embed = new Discord.MessageEmbed();
 
-            embed
-                .setTitle(`\`Pong\`! :ping_pong:`)
-                .setDescription(`:hourglass: **Ping**: \`${client.ws.ping}\` ms.`)
-                .setColor("RANDOM")
-                .setTimestamp();
-            message.channel.send({ embeds: [embed] });
-        } catch (err) {
-            console.log(err);
-        }
-    }
-}
+			embed
+				.setTitle('`Pong`! :ping_pong:')
+				.setDescription(`:hourglass: **Ping**: \`${client.ws.ping}\` ms.`)
+				.setColor('RANDOM')
+				.setTimestamp();
+			message.channel.send({ embeds: [embed] });
+		}
+		catch (err) {
+			console.log(err);
+		}
+	},
+};
