d8ea2ac17463992535991f546cc1890086991b2c
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-12 13:23:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.distube = new DisTube(client, {
 	emitAddSongWhenCreatingQueue: true,
 	emitAddListWhenCreatingQueue: true,
 	updateYouTubeDL: true,
-	youtubeCookie: 'VISITOR_INFO1_LIVE=o_UFlrL8LuI; LOGIN_INFO=AFmmF2swRAIgWxkpYLeeVW5Om-nQRYt1_63-Q-adkihAnZxnuusfQdcCICOKoF0v_cr_r7kAuO7nvtfNyTaS6rJpJjLz_eX8O97e:QUQ3MjNmeGJDSjFmS25PbjZ3aWtZRXZZRXFyT1A4T1BDNGJuUmZBT1N5NjlQekcyOWYxNEpwU1pqaGotQlpWMmJJTVpGLUUtZEhUN1BvSWlTbEdwVnB3bGJvS0RKTWFIbi1hZmlOdnlTUkF1VUx2Zno5QkZyMTc3ZndkYURjbWQxd25MNVlvR0c3aTZMNXk3ckhMZ01KbFhCVVVhUnpIOXdXeGFXUDdfT1ljSE4wZXpOa3d5UzFiZkMwcDZ0eUtqckpuUElROHl2Z3BmSlpYeEpvRU5pazhsS1BXemFuRjJHUQ==; HSID=AaMTSJNlGTqsYvMaO; SSID=AXwA400TeBpyJw0VF; APISID=-3e8o4gYZt8BhMK_/ApwB0adqReqtVlrG8; SAPISID=GP4MSIm469YWkkaP/AaSw_M-WrCr9lhQSG; __Secure-1PAPISID=GP4MSIm469YWkkaP/AaSw_M-WrCr9lhQSG; __Secure-3PAPISID=GP4MSIm469YWkkaP/AaSw_M-WrCr9lhQSG; SID=GgjBo5_RNpuUHU2gzIwSOAy-lsCrg3KTpenbit874pAPXQABk2Lri7kMeWYj0ZKz6LiK9g.; __Secure-1PSID=GgjBo5_RNpuUHU2gzIwSOAy-lsCrg3KTpenbit874pAPXQAB6YxmOpWV_udLzF9tD8JGDA.; __Secure-3PSID=GgjBo5_RNpuUHU2gzIwSOAy-lsCrg3KTpenbit874pAPXQABu9idS-oFKayUO2t89-jb1Q.; PREF=tz=Asia.Bangkok&f6=40000000&f5=20000&f4=4000000; YSC=9a_P9XCjYMA; SIDCC=AJi4QfEmDGkM_qN4R8u6KToQDRJz7tz1nKfp-mPrsz-vFghzd8OrynjAqfY5QaU9lQ1FaLdPnasq; __Secure-3PSIDCC=AJi4QfGSwCGDFkwc7V-AQKw4s2-aUblFt2B5zCwhjybusUIH19KEpXIR-axrIBJGS5L-5oTJ7_g',
+	youtubeCookie: process.env.YOUTUBECOOKIE,
 	plugins: [
 		new SpotifyPlugin({
 			parallel: true,
