fa5665a4c0479071b8aa1a5f0a7ca63edf7cecad
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-12 23:18:23 +0700

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -1,28 +1,34 @@
-const config = require(`../../config.json`)
-const client = require(`../../index.js`)
-const Discord = require(`discord.js`)
+const config = require('../../config.json');
+const client = require('../../index.js');
+const Discord = require('discord.js');
 
-client.on("messageCreate", async message => {
-    //ko cho bot khác sử dụng bot
-    if (message.author.bot) return;
-    //không cho người dùng sử dụng bot trong direct message 
-    if (!message.guild) return;
-    const prefix = config.prefix;
-    //nếu tin nhắn không bắt đầu với prefix thì bỏ luôn
-    if (!message.content.startsWith(prefix)) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if (message.channel.partial) await message.channel.fetch();
-    if (message.partial) await message.fetch()
-    // getting prefix when bot mention
-    if (cmd.leength === 0) return;
-    let command = client.commands.get(cmd);
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
-    if (command) {
-        if (command.category === 'music' && !message.member.voice.channel) return message.channel.send({
-            embeds: [
-                new Discord.MessageEmbed().setColor("RANDOM").setDescription('❌ | **You must be in a voice channel to use the bot!**')],
-        });
-        command.run(client, message, args);
-    }
-})
+client.on('messageCreate', async message => {
+	// ko cho bot khác sử dụng bot
+	if (message.author.bot) return;
+	// không cho người dùng sử dụng bot trong direct message
+	if (!message.guild) return;
+	const prefix = config.prefix;
+	// nếu tin nhắn không bắt đầu với prefix thì bỏ luôn
+	if (!message.content.startsWith(prefix)) return;
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+	if (message.channel.partial) await message.channel.fetch();
+	if (message.partial) await message.fetch();
+	if (cmd.leength === 0) return;
+	let command = client.commands.get(cmd);
+	if (!command) command = client.commands.get(client.aliases.get(cmd));
+	if (command) {
+		if (command.category === 'music' && !message.member.voice.channel) {
+			return message.channel.send({
+				embeds: [
+					new Discord.MessageEmbed().setColor('RANDOM').setDescription('❌ | **You must be in a voice channel to use the bot!**')],
+			});
+		}
+		command.run(client, message, args);
+	}
+    // voiceconnection
+	const voiceChannel = message.member.voice.channel;
+	if (!message.guild.me.voice.channel) {
+		await message.channel.send(`📣 Successfully connected to channel  **${voiceChannel.name}**`);
+	}
+});
