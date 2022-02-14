2efd5b1c0ff8470c51b0ee961dae925613f17a83
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-14 21:37:25 +0700

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -9,14 +9,16 @@ module.exports = {
 	description: 'Shows info of the user',
 	usage: `${config.prefix}userinfo`,
 	run: async (client, message, args) => {
+		const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 		try {
 			axios
-				.get('http://www.sagiri-fansub.tk/api/v1/endpoint')
+				.get(`https://sagiri-fansub.tk/api/v1/userinfo/${member.user.id}`)
 				.then((res) => {
-					const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+					// console.log(res.data.data);
 					const embed = new MessageEmbed()
-						.setAuthor(
-							'User Info',
+						.setAuthor({
+							name: 'User Info',
+						},
 						)
 						.setTitle(`${member.user.tag}`)
 						.setColor('PURPLE')
@@ -25,12 +27,12 @@ module.exports = {
 						.setFields(
 							{
 								name: 'UserID',
-								value: `${member.user.id}`,
+								value: `╰ ${member.user.id}`,
 								inline: true,
 							},
 							{
 								name: 'Tag',
-								value: `${member}`,
+								value: `╰ ${member}`,
 								inline: true,
 							},
 							{
@@ -40,37 +42,67 @@ module.exports = {
 							},
 							{
 								name: 'Status',
-								value: `${member.presence?.status}`,
+								value: `╰ ${member.presence?.status}`,
 								inline: true,
 							},
 							{
 								name: 'HypeSquad',
-								value: `${member.user.flags.toArray()}`,
+								value: `╰ ${member.user.flags.toArray()}`,
 								inline: true,
 							},
 							{
 								name: 'Acc created at',
-								value: `${member.user.createdAt}`,
+								value: `╰ ${member.user.createdAt}`,
 								inline: true,
 							},
 							{
 								name: 'Joined at',
-								value: `${member.joinedAt}`,
+								value: `╰ ${member.joinedAt}`,
 								inline: true,
 							},
 							{
 								name: 'Roles',
-								value: `${member.roles.cache.map(r => r).join(', ')}`,
+								value: `╰ ${member.roles.cache.map(r => r).join(', ')}`,
 								inline: true,
 							},
 							{
-								name: 'Currently Doing',
-								value: `${member.presence?.activities}`,
+								name: 'Currently',
+								value: `╰ ${member.presence?.activities}`,
 								inline: true,
 							},
 							{
 								name: 'Facebook',
-								value: `${res.data}`,
+								value: `╰ ${res.data.data.connected_accounts.map(fb => fb.name)[0]}`,
+								inline: true,
+							},
+							{
+								name: 'GitHub',
+								value: `╰ ${res.data.data.connected_accounts.map(gh => gh.name)[1]}`,
+								inline: true,
+							},
+							{
+								name: 'Reddit',
+								value: `╰ ${res.data.data.connected_accounts.map(rd => rd.name)[2]}`,
+								inline: true,
+							},
+							{
+								name: 'Steam',
+								value: `╰ ${res.data.data.connected_accounts.map(st => st.name)[3]}`,
+								inline: true,
+							},
+							{
+								name: 'Twitch',
+								value: `╰ ${res.data.data.connected_accounts.map(tc => tc.name)[4]}`,
+								inline: true,
+							},
+							{
+								name: 'Twitter',
+								value: `╰ ${res.data.data.connected_accounts.map(tw => tw.name)[5]}`,
+								inline: true,
+							},
+							{
+								name: 'Youtube',
+								value: `╰ ${res.data.data.connected_accounts.map(yt => yt.name)[6]}`,
 								inline: true,
 							},
 						);
