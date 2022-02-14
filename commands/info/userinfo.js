ccd75f3b32c16a037a2f14cad0715e590ea9e45f
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-14 17:52:16 +0700

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const config = require('../../config.json');
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 module.exports = {
 	name: 'userinfo',
@@ -10,65 +10,75 @@ module.exports = {
 	usage: `${config.prefix}userinfo`,
 	run: async (client, message, args) => {
 		try {
-			const { file } = await fetch('http://www.sagiri-fansub.tk/api/v1/endpoint').then(response => response.json());
-			const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-			const embed = new MessageEmbed()
-				.setAuthor(
-					'User Info',
-				)
-				.setTitle(`${member.user.tag}`)
-				.setColor('PURPLE')
-				.setThumbnail(member.user.displayAvatarURL({ dynamic:true }))
-				.setTimestamp()
-				.setFields(
-					{
-						name: 'UserID',
-						value: `${member.user.id}`,
-						inline: true,
-					},
-					{
-						name: 'Tag',
-						value: `${member}`,
-						inline: true,
-					},
-
-					{
-						name: 'Avatar',
-						value: `**[Download Here](${member.user.displayAvatarURL({ dynamic:true })})**`,
-						inline: true,
-					},
-					{
-						name: 'Status',
-						value: `${member.presence?.status}`,
-						inline: true,
-					},
-					{
-						name: 'HypeSquad',
-						value: `${member.user.flags.toArray()}`,
-						inline: true,
-					},
-					{
-						name: 'Acc created at',
-						value: `${member.user.createdAt}`,
-						inline: true,
-					},
-					{
-						name: 'Joined at',
-						value: `${member.joinedAt}`,
-						inline: true,
-					},
-					{
-						name: 'Roles',
-						value: `${member.roles.cache.map(r => r).join(', ')}`,
-						inline: true,
-					},
-					{
-						name: 'Currently Doing',
-						value: `${member.presence?.activities}`,
-						inline: true,
-					},
-				);
-			message.channel.send({ embeds: [embed] });
+			axios
+				.get('http://www.sagiri-fansub.tk/api/v1/endpoint')
+				.then((res) => {
+					const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+					const embed = new MessageEmbed()
+						.setAuthor(
+							'User Info',
+						)
+						.setTitle(`${member.user.tag}`)
+						.setColor('PURPLE')
+						.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+						.setTimestamp()
+						.setFields(
+							{
+								name: 'UserID',
+								value: `${member.user.id}`,
+								inline: true,
+							},
+							{
+								name: 'Tag',
+								value: `${member}`,
+								inline: true,
+							},
+							{
+								name: 'Avatar',
+								value: `**[Download Here](${member.user.displayAvatarURL({ dynamic: true })})**`,
+								inline: true,
+							},
+							{
+								name: 'Status',
+								value: `${member.presence?.status}`,
+								inline: true,
+							},
+							{
+								name: 'HypeSquad',
+								value: `${member.user.flags.toArray()}`,
+								inline: true,
+							},
+							{
+								name: 'Acc created at',
+								value: `${member.user.createdAt}`,
+								inline: true,
+							},
+							{
+								name: 'Joined at',
+								value: `${member.joinedAt}`,
+								inline: true,
+							},
+							{
+								name: 'Roles',
+								value: `${member.roles.cache.map(r => r).join(', ')}`,
+								inline: true,
+							},
+							{
+								name: 'Currently Doing',
+								value: `${member.presence?.activities}`,
+								inline: true,
+							},
+							{
+								name: 'Facebook',
+								value: `${res.data}`,
+								inline: true,
+							},
+						);
+					message.channel.send({ embeds: [embed] });
+				})
+				.catch((err) => {
+					console.error('ERR:', err);
+				});
 		}
 		catch (err) {
 			console.log(err);
