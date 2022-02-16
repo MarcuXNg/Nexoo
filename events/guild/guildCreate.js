4e0710db6b27011d3657e0de29025d1885f19162
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-16 23:00:26 +0700

    Add files via upload

---
+++ events/guild/guildCreate.js
@@ -0,0 +1,33 @@
+const client = require('../../index');
+const { MessageEmbed } = require('discord.js');
+const config = require('../../config.json');
+
+client.on('guildCreate', async (guild) => {
+	const servers = await client.guilds.cache.size;
+	let channelToSend;
+	guild.channels.cache.forEach((channel) => {
+		if (
+			channel.type === 'GUILD_TEXT' &&
+            !channelToSend &&
+            channel.permissionsFor(guild.me).has('SEND_MESSAGES')
+		) channelToSend = channel;
+	});
+
+	if (!channelToSend) return;
+
+	channelToSend.send({
+		embeds: [
+			new MessageEmbed()
+				.setAuthor({
+					name: 'Thank you !',
+				})
+				.setTitle(`Thanks for inviting me to ${guild.name}`)
+				.setDescription(`My prefix is \`${config.prefix}\``)
+				.setColor('RANDOM')
+				.setFooter({
+					text: `Watching \`${servers}\` servers`,
+					iconURL:`${client.user.displayAvatarURL()}` })
+				.setTimestamp(),
+		],
+	});
+});
