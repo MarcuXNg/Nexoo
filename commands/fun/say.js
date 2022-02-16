43225d641a97c4a764c5978edb3ca25c1122fc6a
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-16 17:39:27 +0700

    Update say.js

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -1,17 +1,18 @@
 const config = require('../../config.json');
 module.exports = {
-    name: "say",
-    category: 'fun',
-    aliases: ['say'],
-    description: 'say sth',
-    usage: `${config.prefix}say [sth]`,
-    run: (client, message, args) => {
-        try{
-        if (message.deletable) message.delete();
-        if (!args.length) return message.channel.send(`**\`😖 Nothing to say!\`**`);
-        message.channel.send(args.join(' '));
-        } catch (err) {
-            console.log(err);
-        }
-    }
-}
+	name: 'say',
+	category: 'fun',
+	aliases: ['say'],
+	description: 'say sth',
+	usage: `${config.prefix}say [sth]`,
+	run: (client, message, args) => {
+		try {
+			if (message.deletable) message.delete();
+			if (!args.length) return message.channel.send('**`😖 Nothing to say!`**');
+			message.channel.send(args.join(' '));
+		}
+		catch (err) {
+			console.log(err);
+		}
+	},
+};
