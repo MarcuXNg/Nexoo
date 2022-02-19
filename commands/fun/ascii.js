b3cb74568c5c329921c0e9d432323064a36651c1
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-19 16:44:27 +0700

    Update ascii.js

--- commands/fun/ascii.js
+++ commands/fun/ascii.js
@@ -10,51 +10,56 @@ module.exports = {
 	description: 'Show a random meme',
 	usage: `${config.prefix}ascii <text> <font>`,
 	run : async (client, message, args) => {
-		const text = args[0];
-		const font = args[1];
-		const usage = `\`${config.prefix}ascii <text> <font>\``;
-		const embed = new MessageEmbed();
+		try {
+			const text = args[0];
+			const font = args[1];
+			const usage = `\`${config.prefix}ascii <text> <font>\``;
+			const embed = new MessageEmbed();
 
-		embed
-			.setAuthor({
-				name: client.user.username,
-				iconURL: client.user.displayAvatarURL({ dynamic: true }),
-			})
-			.setDescription(
-				`\`${config.prefix}ascii\`\n
+			embed
+				.setAuthor({
+					name: client.user.username,
+					iconURL: client.user.displayAvatarURL({ dynamic: true }),
+				})
+				.setDescription(
+					`\`${config.prefix}ascii\`\n
                 Converts text into ASCII. A list of fonts can be found at
                 https://github.com/patorjk/figlet.js/tree/master/fonts.\n\n
                 Examples:
                 \`${config.prefix}ascii ghost beep\` - Shows \`beep\` in the \`ghost\` font`,
-			)
-			.setColor(ee.color)
-			.setFields(
-				{
-					name: 'Usage',
-					value: usage,
-					inline: true,
-				},
-				{
-					name: 'Permission Node',
-					value: 'fun.ascii',
-					inline: true,
-				},
-			);
-		if (!text) return message.channel.send({ embeds: [embed] });
-		if (!font) return message.channel.send({ embeds: [embed] });
-		figlet.text(text, {
-			font: font,
-			horizontalLayout: 'default',
-			verticalLayout: 'default',
-			width: 80,
-			whitespaceBreak: true,
-		}, function(err, data) {
-			if (err) {
-				console.log('Something went wrong...');
-				console.dir(err);
-				return;
-			}
-			message.channel.send(`\`\`\` ${data} \`\`\``);
-		});
+				)
+				.setColor(ee.color)
+				.setFields(
+					{
+						name: 'Usage',
+						value: usage,
+						inline: true,
+					},
+					{
+						name: 'Permission Node',
+						value: 'fun.ascii',
+						inline: true,
+					},
+				);
+			if (!text) return message.channel.send({ embeds: [embed] });
+			if (!font) return message.channel.send({ embeds: [embed] });
+			figlet.text(text, {
+				font: font,
+				horizontalLayout: 'default',
+				verticalLayout: 'default',
+				width: 80,
+				whitespaceBreak: true,
+			}, function(err, data) {
+				if (err) {
+					console.log('Something went wrong...');
+					console.dir(err);
+					return;
+				}
+				message.channel.send(`\`\`\` ${data} \`\`\``);
+			});
+		}
+		catch (e) {
+			console.log(e);
+		}
 	},
 };
