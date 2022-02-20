76d0d89af9f63ee7aafb0a6fdb515585def5246a
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-20 15:48:08 +0700

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,24 +3,45 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	run: async (client, message, args) => {
-		console.log(`${client.user.username} ready! Logged in as ${client.user.tag}`);
-		const servers = await client.guilds.cache.size;
-		const membercount = await client.guilds.cache.map(c => c.memberCount).reduce((a, b) => a + b);
-		const activities = [
-			`${config.prefix}help | ${servers} servers`,
-			`Invite me now! | Watching ${membercount} members`,
-			`${client.channels.cache.size} channels`,
-			`My prefix is ${config.prefix}`,
-		];
-		setInterval(() => {
-			const status = activities[Math.floor(Math.random() * activities.length)];
-			client.user.setPresence({
-				activities: [{
-					name: `${status}`,
+		try {
+			const servers = await client.guilds.cache.size;
+			const membercount = await client.guilds.cache.map(c => c.memberCount).reduce((a, b) => a + b);
+			const statusArray = [
+				{
 					type: 'LISTENING',
-				}],
-				status: 'online',
-			});
-		}, 5000);
+					content: `My prefix is ${config.prefix}`,
+					status: 'online',
+				},
+				{
+					type: 'WATCHING',
+					content: `${client.channels.cache.size} channels`,
+					status: 'invisible',
+				},
+				{
+					type: 'PLAYING',
+					content: `${config.prefix}help | ${servers} servers`,
+					status: 'idle',
+				},
+				{
+					type: 'COMPETING',
+					content: `Invite me now! | Watching ${membercount} members`,
+					status: 'dnd',
+				},
+			];
+			setInterval(() => {
+				const option = Math.floor(Math.random() * statusArray.length);
+				client.user.setPresence({
+					activities: [{
+						name: statusArray[option].content,
+						type: statusArray[option].type,
+					}],
+					status: statusArray[option].status,
+				});
+			}, 1000);
+			console.log(`${client.user.username} ready! Logged in as ${client.user.tag}`);
+		}
+		catch (e) {
+			console.log(e);
+		}
 	},
 };
