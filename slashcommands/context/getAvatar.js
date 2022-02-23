432974603f2b8841efde5de7cd2b3f8b2e44f796
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-23 14:13:02 +0700

    Update getAvatar.js

--- slashcommands/context/getAvatar.js
+++ slashcommands/context/getAvatar.js
@@ -3,5 +3,5 @@ const { ContextMenuCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new ContextMenuCommandBuilder()
 		.setName('avatar')
-		.setType(2),
+		.setType(3),
 };
