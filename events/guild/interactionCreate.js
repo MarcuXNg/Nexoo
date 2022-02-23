5a8996e0efad04c90329b6b040e7faf0067343e8
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-23 15:36:13 +0700

    Update interactionCreate.js

--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -12,4 +12,15 @@ client.on('interactionCreate', async interaction => {
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
 	}
+	if (interaction.isContextMenu()) {
+		const slashCommand = client.slashCommands.get(interaction.commandName);
+		if (!slashCommand) return;
+		try {
+			await slashCommand.execute(client, interaction);
+		}
+		catch (e) {
+			console.error(e);
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
