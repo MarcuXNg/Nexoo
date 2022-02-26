09eec8576ddf2dbf798665a2da420caf9fddbe4e
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-26 14:08:50 +0700

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -81,6 +81,7 @@ client.on('messageCreate', async (message) => {
 			userID: message.author.id,
 		});
 		if (profile) return message.channel.send('You cannot use the command as you are banned from using the bot');
+		// run the command
 		command.run(client, message, args);
 	}
 });
