505c977e2113ba998efa058dccc0f2d33240b0db
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-26 11:15:20 +0700

    Update coinflip.js

--- commands/fun/coinflip.js
+++ commands/fun/coinflip.js
@@ -6,29 +6,13 @@ module.exports = {
 	description: 'flip a coin',
 	usage: '(prefix)coinflip',
 	run : async (client, message) => {
-		try {
-			const outcomesIndex = Math.round(Math.random());
-			if (outcomesIndex === 0) {
-				message.channel.send({
-					embeds: [
-						new MessageEmbed()
-							.setTitle('Heads!')
-							.setImage('https://static.ayana.io/commands/flipcoin/heads.png'),
-					],
-				});
-			}
-			if (outcomesIndex === 1) {
-				message.channel.send({
-					embeds: [
-						new MessageEmbed()
-							.setTitle('Tails!')
-							.setImage('https://static.ayana.io/commands/flipcoin/tails.png'),
-					],
-				});
-			}
-		}
-		catch (e) {
-			console.log(e);
-		}
+		const outcomesIndex = Math.floor(Math.random() * 2);
+		message.channel.send({
+			embeds: [
+				new MessageEmbed()
+					.setTitle(outcomesIndex == 0 ? 'Head' : 'Tail')
+					.setImage(`https://static.ayana.io/commands/flipcoin/${outcomesIndex == 0 ? 'heads' : 'tails'}.png`),
+			],
+		});
 	},
 };
