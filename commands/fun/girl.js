c555927a7a145f395f45d6e39950cd1fcb5ee320
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-02-28 22:54:34 +0700

    Add files via upload

---
+++ commands/fun/girl.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'girl',
+	aliases: ['girl'],
+	category: 'fun',
+	description: 'random girls pic',
+	usage: '(config)girl',
+	run: async (client, message) => {
+		try {
+			fetch(
+				'https://www.sagiri-fansub.tk/api/v1/girl',
+			).then(res => {
+				res.json().then(result => {
+					message.channel.send({ embeds: [
+						new MessageEmbed()
+							.setColor('RANDOM')
+							.setImage(result.data.proxyUrl),
+					],
+					});
+				}).catch(err => {
+					console.log(err);
+				});
+			});
+		}
+		catch (err) {
+			console.log(err);
+		}
+	},
+};
+
