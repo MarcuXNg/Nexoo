2a581d9d32c917f9b24c64c2539dadbd4f72111e
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-03-13 00:43:56 +0700

    Add files via upload

---
+++ events/role/roleDelete.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require('discord.js');
+const c = require('../../index');
+const Guild = require('../../database/models/guildSchema');
+
+c.on('roleDelete', async function(role) {
+	send_log(c,
+		role.guild,
+		'RED',
+		`🗑 Role deleted: ${role.name}`,
+		`**RoleID:** ${role.id}\n**Hexcolor:** ${role.hexColor}\n**Mentionable:** ${role.mentionable}\n**Hoisted:** ${role.hoist}`,
+	);
+});
+
+async function send_log(client, guild, color, title, description, thumb) {
+	const guildProfile = await Guild.findOne({ guildID: guild.id });
+	const LogEmbed = new MessageEmbed()
+		.setColor(color ? color : 'BLACK')
+		.setDescription(description ? description.substr(0, 2048) : '\u200b')
+		.setTitle(title ? title.substr(0, 256) : '\u200b')
+		.setTimestamp()
+		.setThumbnail(thumb ? thumb : guild.iconURL({ format: 'png' }))
+		.setFooter({
+			text: 'Powerd By MarcuX and SPARKA',
+		});
+	if (!guildProfile.logChannel) return;
+	const logger = await client.channels.fetch(guildProfile.logChannel);
+
+	logger.send({
+		embeds: [LogEmbed],
+	});
+}
