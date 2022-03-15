676cefea1553763e359d523d1c9a5b5b6db666a2
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-03-16 00:44:54 +0700

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -19,30 +19,31 @@ client.on('guildMemberAdd', async (member) => {
 					.setTimestamp(),
 			],
 		});
+		if (member.user.bot) return;
+		member.send('Thanks you for joining my server ❤️');
 	}
-	else {
-		let channelToSend;
-		member.guild.channels.cache.forEach((channel) => {
-			if (
-				channel.type === 'GUILD_TEXT' &&
-					!channelToSend &&
-					channel.permissionsFor(member.guild.me).has('SEND_MESSAGES')
-			) channelToSend = channel;
-		});
+	// else {
+	// 	let channelToSend;
+	// 	member.guild.channels.cache.forEach((channel) => {
+	// 		if (
+	// 			channel.type === 'GUILD_TEXT' &&
+	// 				!channelToSend &&
+	// 				channel.permissionsFor(member.guild.me).has('SEND_MESSAGES')
+	// 		) channelToSend = channel;
+	// 	});
 
-		if (!channelToSend) return;
-		channelToSend.send({
-			embeds: [
-				new Discord.MessageEmbed()
-					.setColor('RANDOM')
-					.setImage('https://i.pinimg.com/originals/a5/46/45/a54645c9a2150d4278619fbf1a697a91.jpg')
-					.setDescription(`Hey ${member}, welcome to **${member.guild.name}!** \n Hope you have fun here ❤️`)
-					.setFooter({
-						text: `Watching ${servers} servers`,
-					})
-					.setTimestamp(),
-			],
-		});
-	}
-	member.send('Thanks you for joining my server ❤️');
+	// 	if (!channelToSend) return;
+	// 	channelToSend.send({
+	// 		embeds: [
+	// 			new Discord.MessageEmbed()
+	// 				.setColor('RANDOM')
+	// 				.setImage('https://i.pinimg.com/originals/a5/46/45/a54645c9a2150d4278619fbf1a697a91.jpg')
+	// 				.setDescription(`Hey ${member}, welcome to **${member.guild.name}!** \n Hope you have fun here ❤️`)
+	// 				.setFooter({
+	// 					text: `Watching ${servers} servers`,
+	// 				})
+	// 				.setTimestamp(),
+	// 		],
+	// 	});
+	// }
 });
