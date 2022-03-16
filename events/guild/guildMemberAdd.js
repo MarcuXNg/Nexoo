117f09d0eb2227b06371f4d076550d4d8a06ac63
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-03-16 18:45:59 +0700

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -5,22 +5,24 @@ const Guild = require('../../database/models/guildSchema');
 client.on('guildMemberAdd', async (member) => {
 	const servers = client.guilds.cache.size;
 	const guildProfile = await Guild.findOne({ guildID: member.guild.id });
-	if (guildProfile.welcomeChannel) {
-		const channel = member.guild.channels.cache.get(guildProfile.welcomeChannel);
-		channel.send({
-			embeds: [
-				new Discord.MessageEmbed()
-					.setColor('RANDOM')
-					.setImage('https://i.pinimg.com/originals/a5/46/45/a54645c9a2150d4278619fbf1a697a91.jpg')
-					.setDescription(`Hey ${member}, welcome to **${member.guild.name}!** \n Hope you have fun here ❤️`)
-					.setFooter({
-						text: `Watching ${servers} servers`,
-					})
-					.setTimestamp(),
-			],
-		});
-		if (member.user.bot) return;
-		member.send('Thanks you for joining my server ❤️');
+	if (guildProfile.welcome == true) {
+		if (guildProfile.welcomeChannel) {
+			const channel = member.guild.channels.cache.get(guildProfile.welcomeChannel);
+			channel.send({
+				embeds: [
+					new Discord.MessageEmbed()
+						.setColor('RANDOM')
+						.setImage('https://i.pinimg.com/originals/a5/46/45/a54645c9a2150d4278619fbf1a697a91.jpg')
+						.setDescription(`Hey ${member}, welcome to **${member.guild.name}!** \n Hope you have fun here ❤️`)
+						.setFooter({
+							text: `Watching ${servers} servers`,
+						})
+						.setTimestamp(),
+				],
+			});
+			if (member.user.bot) return;
+			member.send(`Thanks you for joining ${member.guild.name} ❤️`);
+		}
 	}
 	// else {
 	// 	let channelToSend;
