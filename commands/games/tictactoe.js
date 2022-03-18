c788e288ded7ee2d8637bdb75c18bd2f6852a6b6
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-03-18 23:01:39 +0700

    Add files via upload

---
+++ commands/games/tictactoe.js
@@ -0,0 +1,22 @@
+const Tictactoe = require('discord-tictactoe');
+
+const game = new Tictactoe({
+	languague: 'en',
+});
+
+module.exports = {
+	name: 'tictactoe',
+	aliases: ['ttt'],
+	description: 'Tictactoe game',
+	category: 'tictactoe',
+	usage: '(prefix)tictactoe',
+	run: async (client, message, args) => {
+		try {
+			const people = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message;
+			game.handleMessage(people);
+		}
+		catch (err) {
+			console.log(err);
+		}
+	},
+};
