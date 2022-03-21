51efcb6a2bfac2f71f99936fa5343a789a426b64
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-03-21 19:53:18 +0700

    Update blacklist.js

--- commands/moderation/blacklist.js
+++ commands/moderation/blacklist.js
@@ -2,28 +2,33 @@ const config = require('../../config.json');
 const Blacklist = require('../../database/models/blackListSchema');
 
 module.exports = {
-	name: 'deblacklist',
+	name: 'blacklist',
 	category: 'moderation',
-	aliases: ['deblacklist'],
-	description: 'Unban an user from using the bot',
+	aliases: ['blacklist'],
+	description: 'Ban an user from using the bot',
 	devOnly: true,
 	usage: `${config.prefix}blacklist @member`,
 	run: async (client, message, args) => {
 		try {
 			// blacklist @member reason
 			const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+			const reason = args.slice(1).join(' ') || 'No reason given.';
 
 			if (!args[0]) return message.channel.send('You need to provide a member to blacklist along with why you ban them.');
 			if (!mentionedMember) return message.channel.send('The member stated is not in the server');
 
-			const profile = await Blacklist.findOne({
+			let profile = await Blacklist.findOne({
 				userID: mentionedMember.user.id,
 			});
 
-			if (!profile) return message.channel.send('This member is not banned from using the bot.');
+			if (profile) return message.channel.send('This member is already banned from using the bot.');
+			profile = await new Blacklist({
+				userID: mentionedMember.user.id,
+				reason: reason,
+			});
 			try {
-				await Blacklist.findOneAndDelete({ userID: mentionedMember.user.id });
-				message.channel.send(`Banned ${mentionedMember.user.tag} from using the bot.`);
+				await profile.save();
+				message.channel.send(`Unbanned ${mentionedMember.user.tag} from using the bot.`);
 			}
 			catch (e) {
 				console.log(e);
