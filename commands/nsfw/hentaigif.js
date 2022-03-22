71f91a5b93344dca457c7c550b5fce5ad1bdd29e
Nguyen Trung Kien <95072521+MarcuXNg@users.noreply.github.com>
2022-03-22 13:46:24 +0700

    Update hentaigif.js

--- commands/nsfw/hentaigif.js
+++ commands/nsfw/hentaigif.js
@@ -1,24 +1,25 @@
 const Discord = require('discord.js');
 const nekoclient = require('nekos.life');
 const neko = new nekoclient();
-const config = require('../../config.json')
+const config = require('../../config.json');
 
 module.exports = {
-  name: "hentaigif",
-  aliases: ["h"],
-  category: "nsfw",
-  description: "Shows a random hentai gif",
-  usage: `${config.prefix}hentaigif`,
-  run: async (client, message, args) => {
-    try {
-      const GIF = await neko.nsfw.randomHentaiGif();
-      message.channel.send({
-        embeds: [
-          new Discord.MessageEmbed().setColor("RANDOM").setImage(GIF.url).setTitle(`That's what ${message.author.username} want`),
-        ],
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  },
+	name: 'hentaigif',
+	aliases: ['hgif'],
+	category: 'nsfw',
+	description: 'Shows a random hentai gif',
+	usage: `${config.prefix}hentaigif`,
+	run: async (client, message, args) => {
+		try {
+			const GIF = await neko.nsfw.randomHentaiGif();
+			message.channel.send({
+				embeds: [
+					new Discord.MessageEmbed().setColor('RANDOM').setImage(GIF.url).setTitle(`That's what ${message.author.username} want`),
+				],
+			});
+		}
+		catch (e) {
+			console.log(e);
+		}
+	},
 };
